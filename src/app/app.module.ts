e40729721a0ecc0e30c44bca606b53ff5003fe45
Lautaro Piugh <lautapiugh@gmail.com>
2022-07-27 05:36:37 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadComponent } from './componets/head/head.component';
+import { LogoComponent } from './componets/logo/logo.component';
+import { SocialmediaComponent } from './componets/socialmedia/socialmedia.component';
+import { BannerComponent } from './componets/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    LogoComponent,
+    SocialmediaComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
