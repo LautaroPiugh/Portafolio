4e2acb18c9d1390bf1e6b9518f001a11945122f7
Lautaro Piugh <lautapiugh@gmail.com>
2022-07-30 20:49:26 -0300

    backend
    se crea la parte de backend

---
+++ BackEnd/src/main/java/com/portafolio/lautapiugh/LautapiughApplication.java
@@ -0,0 +1,13 @@
+package com.portafolio.lautapiugh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LautapiughApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LautapiughApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/portafolio/lautapiugh/LautapiughApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.lautapiugh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LautapiughApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
