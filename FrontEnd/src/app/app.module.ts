74a467fe8f4ab3bc62b5365c89321a72839c5f51
Lautaro Piugh <lautapiugh@gmail.com>
2022-07-30 20:13:32 -0300

    FrontEnd
    se creo la carpeta frontend

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 import { HeadComponent } from './componets/head/head.component';
 import { LogoComponent } from './componets/logo/logo.component';
 import { SocialmediaComponent } from './componets/socialmedia/socialmedia.component';
 import { BannerComponent } from './componets/banner/banner.component';
+import { AcercadeComponent } from './componets/acercade/acercade.component';
+import { ExperienciaComponent } from './componets/experiencia/experiencia.component';
+import { EducacionComponent } from './componets/educacion/educacion.component';
+
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { PpComponent } from './componets/pp/pp.component';
+import { FooterComponent } from './componets/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +23,17 @@ import { BannerComponent } from './componets/banner/banner.component';
     HeadComponent,
     LogoComponent,
     SocialmediaComponent,
-    BannerComponent
+    BannerComponent,
+    AcercadeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    PpComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ FrontEnd/src/app/componets/hard-soft/hard-soft.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
+@Component({
+  selector: 'app-hard-soft',
+  templateUrl: './hard-soft.component.html',
+  styleUrls: ['./hard-soft.component.css']
+})
+export class HardSoftComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
